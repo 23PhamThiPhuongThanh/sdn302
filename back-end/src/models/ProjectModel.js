900019d6b933cd62d0ec875f8160f8a649945576
23PhamThiPhuongThanh <thanhptphe173367@fpt.edu.vn>
2025-03-11 19:27:37 +0700

    update project crud
    update project crud

--- back-end/src/models/ProjectModel.js
+++ back-end/src/models/ProjectModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+<<<<<<< Updated upstream
 const projectSchema = new mongoose.Schema(
     {
         project_name: { type: String, required: [true, 'Project name is require'] },
@@ -10,6 +11,17 @@ const projectSchema = new mongoose.Schema(
         project_end: { type: Date, required: true },
     } 
 );
+=======
+const projectSchema = new mongoose.Schema({
+    project_code: { type: String, required: true, unique: true },
+    project_name: { type: String, required: true },
+    mentor_id: { type: String, required: true },
+    project_detail: { type: String, required: true },
+    project_img: { type: String, required: true },
+    project_start_day: { type: Date, required: true },
+    project_end_day: { type: Date, required: true }
+});
+>>>>>>> Stashed changes
 
 const Project = mongoose.model("Project", projectSchema);
 module.exports = Project;
--- back-end/src/routes/ProjectRouter.js
+++ back-end/src/routes/ProjectRouter.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const express = require('express')
 const router = express.Router();
 const projectController = require("../controllers/ProjectController");
@@ -10,5 +11,14 @@ router.get("/list-project-detail/:id", projectController.listProjectDetail);
 router.get("/search-project-by-name/:name", projectController.searchProjectByName);
 router.put("/update-project/:id", authAdminMiddleware, projectController.updateProject);
 router.delete("/delete-project/:id", authAdminMiddleware, projectController.deleteProject);
+=======
+const express = require("express");
+const router = express.Router();
+const ProjectController = require("../controllers/ProjectController");
+
+router.get("/", ProjectController.getAllProject);
+router.get("/get-details/:id", ProjectController.getDetailsProject);
+router.get("/search", ProjectController.searchProjectByName);
+>>>>>>> Stashed changes
 
 module.exports = router;
